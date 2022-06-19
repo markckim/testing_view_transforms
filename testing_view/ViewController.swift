f4432568b97998174e2d31a16dc19dcbfd101199
Mark Kim <mkim1030@gmail.com>
2022-06-19 15:28:55 -0700

    Initial Commit

---
+++ testing_view/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testing_view
+//
+//  Created by Mark Kim on 6/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
