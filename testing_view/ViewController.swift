a533ac67b1ba2fa8715b6dfe63a2f8b5dbf9b8a9
Mark Kim <mkim1030@gmail.com>
2022-06-19 16:23:35 -0700

    initial commit

--- testing_view/ViewController.swift
+++ testing_view/ViewController.swift
@@ -9,11 +9,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var testView: UIView?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        createAndSetupTestView()
+    }
+    func createAndSetupTestView() {
+        let width = CGFloat(300)
+        let height = CGFloat(200)
+        let frame = CGRectMake(0.5 * (view.bounds.width - width), 0.5 * (view.bounds.height - height), width, height)
+
+        let testView = LabelView(frame: frame)
+        testView.title = "Testing"
+        view.addSubview(testView)
+
+        self.testView = testView
     }
 
+    @IBAction func didTapTestButton(_ sender: Any) {
+        guard let testView = testView else {
+            return
+        }
 
+        // position
+        let initialTranslation = CGAffineTransform(translationX: 100, y: -200)
+        let initialScale = CGAffineTransform(scaleX: 0.1, y: 0.1)
+        let combinedInitialTransform = initialScale.concatenating(initialTranslation)
+
+        testView.transform = combinedInitialTransform
+
+        UIView.animate(withDuration: 0.5) {
+            testView.transform = CGAffineTransform(scaleX: 1, y: 1)
+        } completion: { success in
+            // do stuff
+        }
+    }
 }
-
